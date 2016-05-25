955accf2891c8420e14e36ba5e30bf9d04e9850c
Rodrigo Freitas <rsfreitas@users.noreply.github.com>
2016-05-25 16:37:17 -0300

    Add function to print data as hexadecimal strings
    - Adding code to function that prints data as hexadecimal strings, to be
      used to print binary data.
    
    - Adding validation to arguments while trying to open a log file.

--- src/log.c
+++ src/log.c
@@ -27,22 +27,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
+#include <ctype.h>
 
 #include <pthread.h>
 
 #include "collections.h"
 
+/* Structure used to store the last message written in the log file */
 struct last_msg {
     cstring_t       *msg;
     unsigned int    count;
 };
 
+/*
+ * A list to hold all of the prefixes that will be written before the
+ * messages.
+ */
 struct log_fmt {
     struct log_fmt      *prev;
     struct log_fmt      *next;
     enum clog_fmt_field field;
 
-    /* TODO: Add priority */
+    /* TODO: Add priority? */
 };
 
 struct clog_fmt_s {
@@ -65,6 +71,45 @@ struct clog_s {
     struct ref_s        ref;
 };
 
+static void close_log_file(struct clog_s *log);
+
+static bool is_mode_valid(enum clog_mode mode)
+{
+    switch (mode) {
+        case CLOG_SYNC_ALL_MSGS:
+        case CLOG_KEEP_FILE_OPEN:
+            return true;
+    }
+
+    return false;
+}
+
+static bool is_level_valid(enum clog_level level)
+{
+    switch (level) {
+        case CLOG_OFF:
+        case CLOG_EMERG:
+        case CLOG_ALERT:
+        case CLOG_CRITI:
+        case CLOG_ERROR:
+        case CLOG_WARNG:
+        case CLOG_NOTICE:
+        case CLOG_INFO:
+        case CLOG_DEBUG:
+            return true;
+    }
+
+    return false;
+}
+
+static bool is_separator_valid(char separator)
+{
+    if (ispunct(separator))
+        return true;
+
+    return false;
+}
+
 /*
  * Releases a 'struct clog_s' from memory. Function to be called when its
  * reference count drops to 0.
@@ -85,8 +130,8 @@ static void __destroy_clog_s(const struct ref_s *ref)
     if (l->pathname != NULL)
         free(l->pathname);
 
-    if (l->f != NULL)
-        fclose(l->f);
+    if (l->mode == CLOG_KEEP_FILE_OPEN)
+        close_log_file(l);
 
     free(l);
 }
@@ -183,31 +228,48 @@ static struct clog_fmt_s *new_clog_fmt_s(void)
     return l;
 }
 
+static int open_log_file(struct clog_s *log)
+{
+    log->f = fopen(log->pathname, "r+");
+
+    if (NULL == log->f) {
+        cset_errno(CL_FILE_OPEN_ERROR);
+        return -1;
+    }
+
+    return 0;
+}
+
+static void close_log_file(struct clog_s *log)
+{
+    if (log->f != NULL)
+        fclose(log->f);
+}
+
+static void sync_log_data(struct clog_s *log)
+{
+    if (log->f != NULL)
+        fflush(log->f);
+}
+
 /*
  * Sets the log mode.
  *
  * - CLOG_SYNC_ALL_MSGS: every message will cause the log file to be opened,
- *                       the message will be written and then closed.
+ *                       the message will be written and then the file closed.
  *
- * - CLOG_KEEP_FILE_OPEN: the file will be opened as log as the @log is active
+ * - CLOG_KEEP_FILE_OPEN: the file will be opened as long as the @log is active
  *                        inside the application.
  */
 static int set_log_mode(struct clog_s *log, enum clog_mode mode,
     const char *pathname)
 {
     log->f = NULL;
-    log->pathname = NULL;
+    log->pathname = strdup(pathname);
+    log->mode = mode;
 
-    if (mode == CLOG_SYNC_ALL_MSGS)
-        log->pathname = strdup(pathname);
-    else if (mode == CLOG_KEEP_FILE_OPEN) {
-        log->f = fopen(pathname, "r+");
-
-        if (NULL == log->f) {
-            cset_errno(CL_FILE_OPEN_ERROR);
-            return -1;
-        }
-    }
+    if (mode == CLOG_KEEP_FILE_OPEN)
+        return open_log_file(log);
 
     return 0;
 }
@@ -237,7 +299,33 @@ static cstring_t *set_default_log_header(void)
 static int write_message(struct clog_s *log, enum clog_level level,
     const char *msg)
 {
+    if (is_level_valid(level) == false)
+        return -1;
+
     return 0;
+}
+
+static int write_hex_message(struct clog_s *log, enum clog_level level,
+    void *data, unsigned int dsize)
+{
+    unsigned int i;
+    char *ptr = data;
+
+    if (is_level_valid(level) == false)
+        return -1;
+
+    for (i = 0; i < dsize; i++)
+        fprintf(log->f, "%02x", ptr[i]);
+
+    return 0;
+}
+
+static bool check_log_mode(struct clog_s *log, enum clog_mode mode)
+{
+    if (log->mode == mode)
+        return true;
+
+    return false;
 }
 
 /*
@@ -310,12 +398,18 @@ clog_t *clog_open_ex(const char *pathname, enum clog_mode mode,
 
     cerrno_clear();
 
-    if (NULL == pathname) {
+    if ((NULL == pathname) || (NULL == fmt)) {
         cset_errno(CL_NULL_ARG);
         return NULL;
     }
 
-    /* TODO: validate mode, start_level, max_repeat, separator and fmt */
+    if ((is_mode_valid(mode) == false) ||
+        (is_level_valid(start_level) == false) ||
+        (is_separator_valid(start_level) == false))
+    {
+        cset_errno(CL_INVALID_VALUE);
+        return NULL;
+    }
 
     log = new_clog_s();
 
@@ -420,10 +514,33 @@ int clog_printf(clog_t *log, enum clog_level level, const char *fmt, ...)
     return ret;
 }
 
-void clog_bprint(void)
+int clog_bprint(clog_t *log, enum clog_level level, void *data,
+    unsigned int dsize)
 {
+    int ret;
+
+    cerrno_clear();
+
+    if (NULL == log) {
+        cset_errno(CL_NULL_ARG);
+        return -1;
+    }
+
+    if (check_log_mode(log, CLOG_SYNC_ALL_MSGS))
+        if (open_log_file(log) < 0)
+            return -1;
+
+    ret = write_hex_message(log, level, data, dsize);
+
+    if (check_log_mode(log, CLOG_SYNC_ALL_MSGS)) {
+        sync_log_data(log);
+        close_log_file(log);
+    }
+
+    return ret;
 }
 
+/* XXX: rprint? */
 void clog_rprint(void)
 {
 }
