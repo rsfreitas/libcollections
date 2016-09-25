4bdadfe4fc299780a1b96c8f6bef6bac1125803c
Rodrigo Freitas <rsfreitas@users.noreply.github.com>
2016-09-25 16:53:00 -0300

    Updating the library BUILD value

--- include/collections.h
+++ include/collections.h
@@ -30,7 +30,7 @@
 #ifdef LIBCOLLECTIONS_COMPILE
 # define MAJOR_VERSION          0
 # define MINOR_VERSION          1
-# define BUILD                  9
+# define BUILD                  10
 #endif
 
 #ifdef __cplusplus
