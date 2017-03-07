f6433a3d2dffc3bc4bd19f5dec43fa47e6a7bb55
Rodrigo Freitas <rsfreitas@users.noreply.github.com>
2017-03-07 10:30:34 -0300

    Fix the cstring output from a cdatetime_t object
    - It was inserting an invalid counted char inside the cstring_t object.

--- include/collections.h
+++ include/collections.h
@@ -30,7 +30,7 @@
 #ifdef LIBCOLLECTIONS_COMPILE
 # define MAJOR_VERSION          0
 # define MINOR_VERSION          1
-# define BUILD                  26
+# define BUILD                  27
 #endif
 
 #ifdef __cplusplus
